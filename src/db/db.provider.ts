8c74faa0b166b3b3574152325c1c7773a60ef8d2
dsalangin <dsalangin74@gmail.com>
2025-06-01 13:02:15 +0300

    feat: add db

---
+++ src/db/db.provider.ts
@@ -0,0 +1,14 @@
+// src/db/db.provider.ts
+import { Provider } from '@nestjs/common';
+import { DBEntity } from './db-entity';
+import { User } from '../modules/user/entities/user.entity';
+
+export const DB_PROVIDER = 'DB_PROVIDER';
+
+export const dbProvider: Provider = {
+  provide: DB_PROVIDER,
+  useValue: {
+    users: new DBEntity<User>(),
+    // artists: new DBEntity<Artist>(),
+  },
+};
