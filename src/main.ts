cd110a558310be228ec3bf1fa1902cedf1570bb0
dsalangin <dsalangin74@gmail.com>
2025-06-02 23:34:43 +0300

    feat: add reading .env variables

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
+
+const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
